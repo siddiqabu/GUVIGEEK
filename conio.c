316737efcf0de4c4eaacf9838fad400232ab7cf3
siddiqabu <35294835+siddiqabu@users.noreply.github.com>
2018-02-07 15:54:10 +0530

    conio.c

---
+++ conio.c
@@ -0,0 +1,23 @@
+#include <stdio.h>                     
+int main()                       
+{
+int a[10] = { 3,4,7,6,5,1,2,8,10,9 };           
+int n = 10;                              
+ printf("\n\nArray Data : ");                
+ for (int i = 0; i < n; i++)                    
+ {
+ printf(" %d ", a[i]);                 
+ }
+ for (int i = 0; i < n; i++)      
+{
+for (int j = 0; j < n; j++)   
+{
+if (a[j] > a[i])           
+{
+int tmp = a[i];
+a[i] = a[j];            
+a[j] = tmp;             
+}  
+}
+getch();
+}
